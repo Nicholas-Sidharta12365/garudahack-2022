d18f22ad98f5f528f9d228772dcb6027051571ea
Nicholas Sidharta <toxicancm@gmail.com>
2022-08-26 15:57:39 +0700

    login page otw

---
+++ src/components/login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <>
+    <div className="grid grid-cols-2 bg-gradient-to-r from-blue-500 to-cyan-500 h-screen">
+        <div class="align-middle">
+            <div className="text-5xl font-bold font-roboto text-center">Welcome Back!</div>
+            <div className="text-white text-center text-xl mt-2">What could we do for you today?</div>
+        </div>
+        <div className="">
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default login
---
+++ src/pages/loginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../components/login'
+
+const loginPage = () => {
+  return (
+    <>
+    <Login />
+    </>
+  )
+}
+
+export default loginPage
