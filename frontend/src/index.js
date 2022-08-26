8470fa602b4f3a0c33b1ef24f1494f6d0580bac8
JulianTanja <61524227+JulianTanja@users.noreply.github.com>
2022-08-26 22:59:09 +0700

    julian's first commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBf5XGNmobjq6_JhCv8-oY9zi0ig0fO_io",
+  authDomain: "garudahacks-62704.firebaseapp.com",
+  projectId: "garudahacks-62704",
+  storageBucket: "garudahacks-62704.appspot.com",
+  messagingSenderId: "157997345966",
+  appId: "1:157997345966:web:87ef1ce420cdd4e675913f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
