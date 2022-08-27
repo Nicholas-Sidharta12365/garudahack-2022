06c92edb07c17143f13e1fb5cebfe74f44dbaf97
Nicholas Sidharta <toxicancm@gmail.com>
2022-08-27 16:25:58 +0700

    update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,8 +4,9 @@ import {Link} from 'react-router-dom';
 function Footer() {
   return (
     <div className="absolute inset-x-20 bottom-0 h-20">
-        <form className="inline-block align-middle">
+        <form className="inline-block align-middle ml-16">
             <input className="border-solid border-2 border-sky-500 shadow px-20 py-2 rounded-full" type="text" />
+            <button type="submit" className="border rounded-full bg-blue-200 p-2">Send</button>
         </form>
     </div>
   );
