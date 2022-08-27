be67da973cde471be1e332dbb9763e6a00ca65f4
Nicholas Sidharta <toxicancm@gmail.com>
2022-08-27 23:37:26 +0700

    therapist home add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Chat from './pages/Chat';
 import Logout from './pages/Logout';
+import HomeTherapist from './pages/HomeTherapist';
 //import Mainvideo from './components/main';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/join' element={<RegisterTherapist />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/therapist' element={<HomeTherapist />} />
         <Route path='/profile' element={<Profile />}/>
         <Route path='/chat' element={<Chat />}/>
         <Route path='/logout' element={<Logout />}/>
